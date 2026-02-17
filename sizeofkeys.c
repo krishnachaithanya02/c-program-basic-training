2f59b6928151f4ef218efd5f469a90e987ab0231
krishnachaithanya02 <krishnachaithanya0202@gmail.com>
2026-02-17 15:16:49 +0530

    program the size of keywords in c

---
+++ sizeofkeys.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+int main() 
+{
+printf("Size of int: %lu bytes\n", sizeof(int));
+printf("size of char: %lu bytes\n", sizeof(char));
+printf("size of float: %lu bytes\n", sizeof(float));
+printf("size of double: %lu bytes\n", sizeof(double));
+printf("size of long double: %lu bytes\n", sizeof(long double));
+return 0;
+}
+
