0a3e425e00358269ddc7cb3947ab93494368fc7a
krishnachaithanya02 <krishnachaithanya0202@gmail.com>
2026-02-18 17:21:52 +0530

    Add files via upload

---
+++ aab.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+int main() {
+    int x = 25;
+    float p = 19.99;
+    double d = 149600000.5;
+    char i = 'G';
+printf("%d\n", x);
+printf("%.2f\n", p);
+printf("%.1f\n", d);
+printf("%c\n", i);
+
+    return 0;
+}
