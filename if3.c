048a8dc4ef9e66f211c7a6f4dec5cb6be86bd1ea
krishnachaithanya02 <krishnachaithanya0202@gmail.com>
2026-03-02 16:39:58 +0530

    Add files via upload

---
+++ if3.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+int main(){
+int x,y,z;
+printf(" enter a three numbers:");
+scanf("%d %d %d",&x,&y,&z);
+if (x>=y && x>=z) {
+printf("largest number is %d\n",x);
+}
+else if (y>=x && y>=z){
+printf ("largest number is %d\n",y);
+}
+else {
+printf("largest number is %d\n",z);
+}
+return 0;
+}
+
