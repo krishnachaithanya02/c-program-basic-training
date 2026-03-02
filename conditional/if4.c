099e3acec493a72297edca73549c6aa3b24c163a
krishnachaithanya02 <krishnachaithanya0202@gmail.com>
2026-03-02 16:45:45 +0530

    Add files via upload

---
+++ conditional/if4.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+int main() {
+int year;
+printf ("enter a year ");
+scanf ("%d",&year);
+if(year%4==0) {
+printf("it is a leap year\n");
+}
+else {
+printf("it is not a leap year\n");
+}
+return 0;
+}
