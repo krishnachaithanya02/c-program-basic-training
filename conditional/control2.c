6bf4b6a3726bc44ac62b52e2ff4b59d602d022ed
krishnachaithanya02 <krishnachaithanya0202@gmail.com>
2026-03-13 09:53:23 +0530

    Add files via upload

---
+++ conditional/control2.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+int main() {
+int x;
+printf("enter a number");
+scanf("%d",&x);
+if (x%2==0){
+printf("it is a even number");
+}
+else {
+printf("it is a odd");
+}
+return 0;
+}
