a8dab8b99cb3ac8e61f9a620afdd9b45d43350bf
krishnachaithanya02 <krishnachaithanya0202@gmail.com>
2026-03-13 14:26:41 +0530

    Add files via upload

---
+++ conditional/if8.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+int main() {
+int x=5;
+if (x > 0)
+printf("The number is positive.\n");
+else if (x< 0)
+printf("The number is negative.\n");
+else
+printf("The number is zero.\n");
+return 0; }
