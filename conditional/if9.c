f39f8bb5cacd2460ae7a5a243df07660fd5c00a6
krishnachaithanya02 <krishnachaithanya0202@gmail.com>
2026-03-13 15:37:44 +0530

    Add files via upload

---
+++ conditional/if9.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+int main(){
+int x=24;
+if (x>5)
+{
+if(x%5==0){
+printf("it is a positive number: divisible by 5:\n");
+}
+else {
+printf("it is a positive number: not divisible by 5:\n");
+}
+}
+return 0;
+}
+
