5cdbc11abd4261739af97e3f3a0b9788e8d4eb94
krishnachaithanya02 <krishnachaithanya0202@gmail.com>
2026-03-18 13:38:54 +0530

    Add files via upload

---
+++ array/arr4.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+int main(){
+int x[100],n,i,z;
+printf("enter a number of elements:");
+scanf("%d",&n);
+printf("enter a elements:");
+for(i=0;i<n;i++){
+scanf("%d",&x[i]);
+}
+printf("enter elements of search:");
+scanf("%d",&z);
+for(i=0;i<n;i++){
+if(x[i]==z){
+printf("found a number=%d",i);
+return 0;
+}
+}
+printf("element not found");
+
+return 0;
+}
+
