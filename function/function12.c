a34880fa713c601972f98b7841a21eedd34f5ce0
krishna chaithanya <krishnachaithanya0202@gmail.com>
2026-03-28 10:24:15 +0530

    Add files via upload

---
+++ function/function12.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+int reverse(int n)
+{
+int rev=0;
+while(n>0)
+{
+rev=rev*10+(n%10);
+n=n/10;
+}
+return rev;
+}
+int main()
+{
+int result;
+result=reverse(1234);
+printf("reversed no=%d",result);
+return 0;
+}
