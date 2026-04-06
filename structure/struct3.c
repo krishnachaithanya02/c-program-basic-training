fed9fe709ff5ffd9ab615d1c3dad2d8886bc5b40
krishna chaithanya <krishnachaithanya0202@gmail.com>
2026-04-06 17:41:32 +0530

    Add files via upload

---
+++ structure/struct3.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+struct student {
+int id;
+char name[50];
+float marks;
+};
+int main() {
+struct student s[5];
+int i;
+for(i=0;i<5;i++)
+{
+printf("enter details of student %d\n",i+1);
+scanf("%d %s %f",&s[i].id,s[i].name,&s[i].marks);
+}
+printf("\n student details:\n");
+for(i=0;i<5;i++)    {
+printf("%d %s %.2f\n:",s[i].id,s[i].name,s[i].marks);
+}
+return 0;
+}
