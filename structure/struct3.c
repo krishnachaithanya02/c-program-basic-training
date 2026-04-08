967a796a1f41ed6f2d2135c3edc3f7037aaa0354
krishna chaithanya <krishnachaithanya0202@gmail.com>
2026-04-08 16:36:42 +0530

    Update struct3.c

--- structure/struct3.c
+++ structure/struct3.c
@@ -5,15 +5,15 @@ char name[50];
 float marks;
 };
 int main() {
-struct student s[5];
+struct student s[3];
 int i;
-for(i=0;i<5;i++)
+for(i=0;i<3;i++)
 {
 printf("enter details of student %d\n",i+1);
 scanf("%d %s %f",&s[i].id,s[i].name,&s[i].marks);
 }
 printf("\n student details:\n");
-for(i=0;i<5;i++)    {
+for(i=0;i<3;i++)    {
 printf("%d %s %.2f\n:",s[i].id,s[i].name,s[i].marks);
 }
 return 0;
